b1f9df2b8628fb73793c87754b85c5dbb46a9d58
laneevanger <laneevanger@gmail.com>
2026-02-22 05:13:06 -0800

    its 5:12 am
    my magnum opus

--- Hackathon2/Hackathon2/hack.h
+++ Hackathon2/Hackathon2/hack.h
@@ -5,5 +5,14 @@
 #include <ctype.h>
 #include <windows.h>
 
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define PURPLE  "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define WHITE   "\x1b[37m"
+#define RESET   "\x1b[0m"
+
 /* function prototypes */
 void printHat(void);	
--- Hackathon2/Hackathon2/main.c
+++ Hackathon2/Hackathon2/main.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
+    srand(time(NULL));
     printHat();
     return 0;
 }
